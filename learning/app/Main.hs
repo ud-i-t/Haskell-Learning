6f756b8f5a3e92d87ee8d645f09dfb7186bba35b
ud <mizunoawa.web@gmail.com>
2022-05-29 09:09:00 +0900

    ガードまで https://qiita.com/7shi/items/145f1234f8ec2af923ef

--- learning/app/Main.hs
+++ learning/app/Main.hs
@@ -2,5 +2,20 @@ module Main where
 
 import Lib
 
+fact 0 = 1
+fact n | n > 0 = n * fact (n - 1)
+
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
 main = do
-    print "hello world"
+    print c
+    print $ fact 5
+    print $ fib 6
+    print $ fib 7
+    where
+        a = 1
+        b = 2
+        c = a + b
+    
