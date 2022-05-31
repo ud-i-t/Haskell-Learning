646d2c46b89fb48a17f78fbd517faff6bd3745f8
ud <mizunoawa.web@gmail.com>
2022-05-31 22:56:41 +0900

    列挙型

--- learning/app/Main.hs
+++ learning/app/Main.hs
@@ -7,11 +7,18 @@ fib 0 = 0
 fib 1 = 1
 fib n = fib (n - 1) + fib (n - 2)
 
+data Color = Blue | Red | Green | White deriving (Show, Enum)
+
 main = do
     print c
     print $ Fact.fact 5
     print $ fib 6
     print $ fib 7
+    print Blue
+    print $ fromEnum Red
+    print (toEnum 1 :: Color)
+    print (toEnum 2 :: Color)
+
     where
         a = 1
         b = 2
