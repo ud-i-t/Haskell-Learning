50d8c8151fa4edb672809847ace5142be240c8d5
ud <mizunoawa.web@gmail.com>
2022-06-11 18:34:22 +0900

    lesson 3-3

--- learning/learning2/lesson3.hs
+++ learning/learning2/lesson3.hs
@@ -4,3 +4,9 @@ sumSquareOrSquareSum x y = (\sumSquare squareSum ->
                            else squareSum) (x^2 + y^2) ((x+y)^2)
 
 doubleDouble x = (\d -> d * 2) (x * 2)
+
+overwrite x = (\x -> 
+    (\x -> 
+        (\x -> x) 4 
+        ) 3
+    ) 2
