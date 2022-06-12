d06714773994a1296871a951e1bf4688d12110b2
ud <mizunoawa.web@gmail.com>
2022-06-12 19:38:42 +0900

    lesson4

--- learning/learning2/lesson4.hs
+++ learning/learning2/lesson4.hs
@@ -18,21 +18,10 @@ names = [("Ian", "Curtis"),
          ("Peter", "Hooa"),
          ("Stephan", "Morris")]
 
-compareLastNames name1 name2 = if lastName1 > lastName2
-                               then GT 
-                               else if lastName1 < lastName2
-                                   then LT
-                                   else compareFirstNames name1 name2
-    where lastName1 = snd name1
-          lastName2 = snd name2
-
-compareFirstNames name1 name2 = if firstName1 > firstName2
-                                then GT
-                                else if firstName1 < firstName2
-                                    then LT
-                                    else EQ
-    where firstName1 = fst name1
-          firstName2 = fst name2
+compareLastNames name1 name2 = if result == EQ
+                               then compare (fst name1) (fst name2)
+                               else result
+    where result = compare (snd name1) (snd name2)
 
 sfOffice name = if lastName < "L"
                 then nameText
@@ -48,10 +37,14 @@ nyOffice name = nameText ++ ": PO Box 789 - New York, NY, 10013"
 renoOffice name = nameText ++ ": PO Box 456 - Reno, NV, 89523"
     where nameText = snd name
 
+dcOffice name = nameText ++ "- PO Box 5166 - Washington DC, dc, 44941"
+    where nameText = (fst name) ++ " " ++ (snd name) ++ "Esq"
+
 getLocationFunction location = case location of
     "ny" -> nyOffice
     "sf" -> sfOffice
     "reno" -> renoOffice
+    "dc" -> dcOffice
     _ -> (\name -> (fst name) ++ " " ++ (snd name))
 
 addressLetter name location = locationFunction name
