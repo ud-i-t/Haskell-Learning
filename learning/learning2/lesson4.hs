255aabad6d946ac3bb80d9cf6aac00fb5c67857b
ud <mizunoawa.web@gmail.com>
2022-06-12 12:40:31 +0900

    lesson4-2

--- learning/learning2/lesson4.hs
+++ learning/learning2/lesson4.hs
@@ -33,3 +33,26 @@ compareFirstNames name1 name2 = if firstName1 > firstName2
                                     else EQ
     where firstName1 = fst name1
           firstName2 = fst name2
+
+sfOffice name = if lastName < "L"
+                then nameText
+                     ++ " - PO Box 1234 - San Francisco, CA, 94111"
+                else nameText
+                     ++ " - PO Box 1010 - San Francisco, CA, 94109"
+    where lastName = snd name
+          nameText = (fst name) ++ " " ++ lastName
+
+nyOffice name = nameText ++ ": PO Box 789 - New York, NY, 10013"
+    where nameText = (fst name) ++ " " ++ (snd name)
+
+renoOffice name = nameText ++ ": PO Box 456 - Reno, NV, 89523"
+    where nameText = snd name
+
+getLocationFunction location = case location of
+    "ny" -> nyOffice
+    "sf" -> sfOffice
+    "reno" -> renoOffice
+    _ -> (\name -> (fst name) ++ " " ++ (snd name))
+
+addressLetter name location = locationFunction name
+    where locationFunction = getLocationFunction location
