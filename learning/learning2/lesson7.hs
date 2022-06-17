ab616a413c07f6a7c459550c2a98db397b0d2505
ud <mizunoawa.web@gmail.com>
2022-06-17 22:07:16 +0900

    lesson7

---
+++ learning/learning2/lesson7.hs
@@ -0,0 +1,18 @@
+-- lesson7: 再帰
+sayAmount n = case n of
+    1 -> "one"
+    2 -> "two"
+    n -> "a bunch"
+
+sayAmountV2 1 = "one"
+sayAmountV2 2 = "two"
+sayAmountV2 n = "a bunch"
+
+isEmpty [] = True
+isEmpty _ = False
+
+myHead (x:xs) = x
+myHead [] = error "No head for empty list"
+
+myTail (_:xs) = xs
+myTail [] = []
