b78cac6abf5c5fde27743e8baed73be9f6509aa5
ud <mizunoawa.web@gmail.com>
2022-07-04 21:13:36 +0900

    lesson12

---
+++ learning/learning2/lesson12.hs
@@ -0,0 +1,48 @@
+-- lesson12
+-- 型シノニム
+type PatientName = (String,String)
+type Age = Int
+type Height = Int
+
+firstName :: PatientName -> String
+firstName name = fst name
+
+lastName :: PatientName -> String
+lastName name = snd name
+
+patientInfo :: PatientName -> Age -> Height -> String
+patientInfo name age height = fullname ++ " " ++ ageHeight
+    where fullname = (lastName name) ++ ", " ++ (firstName name)
+          ageHeight = "(" ++ show age ++ "yrs. " ++ show height ++ "in.)"
+
+-- 型の作成
+data Sex = Male | Female
+
+sexInitial :: Sex -> Char
+sexInitial Male = 'M'
+sexInitial Female = 'F'
+
+
+data RhType = Pos | Neg
+data ABOType = A | B | AB | O
+data BloodType = BloodType ABOType RhType -- データコンストラクタ
+
+showRh :: RhType -> String
+showRh Pos = "+"
+showRh Neg = "-"
+
+showABO :: ABOType -> String
+showABO A = "A"
+showABO B = "B"
+showABO AB = "AB"
+showABO O = "O"
+
+showBloodType :: BloodType -> String
+showBloodType (BloodType abo rh) = showABO abo ++ showRh rh
+
+patient1BT :: BloodType
+patient1BT = BloodType A Pos
+patient2BT :: BloodType
+patient2BT = BloodType O Neg
+patient3BT :: BloodType
+patient3BT = BloodType AB Pos
