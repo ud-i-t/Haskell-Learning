fe79e679d0b1626eef3232d83666286860220eb1
ud <mizunoawa.web@gmail.com>
2022-07-11 23:02:15 +0900

    lesson13

--- learning/learning2/lesson13.hs
+++ learning/learning2/lesson13.hs
@@ -7,3 +7,13 @@ simple x = x
 -- :info Num
 addThenDouble :: Num a => a -> a -> a
 addThenDouble x y = (x + y)*2
+
+class Describable a where
+    describe :: a -> String
+
+data Icecream = Chocolate | Vanilla deriving (Show, Eq, Ord)
+
+cycleSucc :: (Bounded a, Enum a, Eq a) => a -> a
+cycleSucc n = if n == maxBound
+              then minBound
+              else succ n
