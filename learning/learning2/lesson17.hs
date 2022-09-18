449d370fb45948c2346285229b4de1bccc8fb951
ud <mizunoawa.web@gmail.com>
2022-09-18 10:39:58 +0900

    lesson17

--- learning/learning2/lesson17.hs
+++ learning/learning2/lesson17.hs
@@ -47,3 +47,61 @@ instance Semigroup Color where
 
 
 elemtest = (`elem` [Red, Blue, Purple])
+
+-- monoid
+-- class Monoid a where
+--  mempty :: a (単位元を返す)
+--  mappend :: a -> a -> a (加算)
+--  mconcat :: [a] -> a
+
+type Events = [String]
+type Probs = [Double]
+data PTable = PTable Events Probs
+
+instance Show PTable where
+    show (PTable events probs) = mconcat pairs
+        where pairs = zipWith showPair events probs
+
+instance Semigroup PTable where
+    (<>) ptable1 (PTable [] []) = ptable1
+    (<>) (PTable [] []) ptable2 = ptable2
+    (<>) (PTable e1 p1) (PTable e2 p2) = createPTable newEvents newProbs
+        where newEvents = combineEvents e1 e2
+              newProbs = combineProbs p1 p2
+
+instance Monoid PTable where
+    mempty = PTable [] []
+    mappend = (<>)
+
+createPTable :: Events -> Probs -> PTable
+createPTable events probs = PTable events normalizedProbs
+  where totalProbs = sum probs
+        normalizedProbs = map (\x -> x/totalProbs) probs --全ての確率の和が1になるようにする
+
+showPair :: String -> Double -> String
+showPair event prob = mconcat [event,"|", show prob,"\n"]
+
+cartCombine :: (a -> b -> c) -> [a] -> [b] -> [c]
+cartCombine func l1 l2 = zipWith func newL1 cycledL2
+    where nToAdd = length l2
+          -- . は関数合成, repeatはl1を要素として無限リストを返す
+          repeatdL1 = map (take nToAdd . repeat) l1
+          newL1 = mconcat repeatdL1
+          -- cycle は単一の無限リストを返す
+          cycledL2 = cycle l2
+
+combineEvents :: Events -> Events -> Events
+combineEvents e1 e2 = cartCombine combiner e1 e2
+    where combiner = (\x y -> mconcat [x,"-",y])
+
+combineProbs :: Probs -> Probs -> Probs
+combineProbs p1 p2 = cartCombine (*) p1 p2
+
+-- testData
+coin :: PTable
+coin = createPTable ["heads","tails"] [0.5,0.5]
+
+spinner :: PTable
+spinner = createPTable ["red","blue","green"] [0.1,0.2,0.7]
+
+-- 次は練習問題
