dd5c5919f7ecab763b9150216489bf5b6c13ca65
ud <mizunoawa.web@gmail.com>
2022-09-25 09:18:44 +0900

    lesson18

--- learning/learning2/lesson18.hs
+++ learning/learning2/lesson18.hs
@@ -34,3 +34,31 @@ toList (Triple x y z) = [x,y,z]
 
 transform :: (a -> a ) -> Triple a -> Triple a
 transform f (Triple x y z) = Triple (f x) (f y) (f z)
+
+-- 独自にList型を定義
+-- 再帰になっている
+data List a = Empty | Cons a (List a) deriving Show
+
+ourMap :: (a -> b) -> List a -> List b
+ourMap _ Empty = Empty
+ourMap func (Cons a rest) = Cons (func a) (ourMap func rest)
+
+ourListEx1 :: List Int
+ourListEx1 = Cons 1 (Cons 2 (Cons 3 Empty))
+
+-- タプルのリスト
+itemCount1 :: (String, Int)
+itemCount1 = ("Erasers", 25)
+
+itemCount2 :: (String, Int)
+itemCount2 = ("Pencils", 25)
+
+itemCount3 :: (String, Int)
+itemCount3 = ("Pens", 13)
+
+--型が違うので追加できない
+itemCount4 :: (String, Double)
+itemCount4 = ("Paper", 12.4)
+
+itemInventry :: [(String, Int)]
+itemInventry = [itemCount1, itemCount2, itemCount3]
