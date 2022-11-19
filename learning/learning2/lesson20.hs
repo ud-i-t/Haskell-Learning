88d4aee5b716f3d7b43f34c241458bcd7f50b1b7
ud <mizunoawa.web@gmail.com>
2022-11-19 11:04:51 +0900

    lesson20

--- learning/learning2/lesson20.hs
+++ learning/learning2/lesson20.hs
@@ -21,3 +21,37 @@ file3 = [(10, 201.2), (11, 201.6), (12, 201.5), (13, 201.5), (14, 203.5), (17, 2
 file4 :: [(Int, Double)]
 file4 = [(26, 219.8), (27, 220.5), (28, 223.8), (29, 222.8), (30, 223.8), (31, 221.7),
          (32, 222.3), (33, 220.8), (34, 219.4), (35, 220.1), (36, 220.6)]
+
+data TS a = TS [Int] [Maybe a]
+
+createTS :: [Int] -> [a] -> TS a
+createTS times values = TS completeTimes extenedValues
+    where completeTimes = [minimum times .. maximum times]
+          timeValueMap = Map.fromList (zip times values)
+          extenedValues = map (\v -> Map.lookup v timeValueMap) completeTimes
+
+fileToTS :: [(Int,a)] -> TS a
+fileToTS tvPairs = createTS times values
+    where splitPairs = (unzip tvPairs)
+          times = fst splitPairs
+          values = snd splitPairs
+
+showTVPair :: Show a => Int -> (Maybe a) -> String
+showTVPair time (Just value) = mconcat [show time,"|",show value,"\n"]
+showTVPair time Nothing = mconcat [show time,"|NA\n"]
+
+instance Show a => Show (TS a) where
+    show (TS times values) = mconcat rows
+        where rows = zipWith showTVPair times values
+
+ts1 :: TS Double
+ts1 = fileToTS file1
+
+ts2 :: TS Double
+ts2 = fileToTS file2
+
+ts3 :: TS Double
+ts3 = fileToTS file3
+
+ts4 :: TS Double
+ts4 = fileToTS file4
