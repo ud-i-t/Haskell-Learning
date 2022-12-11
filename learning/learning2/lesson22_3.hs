2f6164c764eebc563ffe09b036dc034b8b829b23
ud <mizunoawa.web@gmail.com>
2022-12-11 10:11:31 +0900

    lesson22

--- learning/learning2/lesson22_3.hs
+++ learning/learning2/lesson22_3.hs
@@ -8,5 +8,10 @@ main = do
     let linesToRead = if length args > 0
                       then read (head args)
                       else 0 :: Int
-    print linesToRead
+    numbers <- replicateM linesToRead getLine
+    let ints = map read numbers :: [Int]
+    print (sum ints)
     
+-- Quick check 22-2
+myReplicateM :: Monad m => Int -> m a -> m [a]
+myReplicateM n func = mapM (\_ -> func) [1 .. n]
