b05c620d7d6785dc4dfb35110594b954c120e67c
ud <mizunoawa.web@gmail.com>
2022-12-12 21:45:56 +0900

    lesson22

---
+++ learning/learning2/lesson22_4.hs
@@ -0,0 +1,17 @@
+-- lesson22
+import System.Environment
+import Control.Monad
+import Data.List.Split
+
+sampleData = ['6', '2', '\n', '2', '1', '\n'] 
+
+myLines lines = splitOn "\n" lines
+
+toInts :: String -> [Int]
+toInts lines = map read (myLines lines)
+
+main :: IO ()
+main = do
+    userInput <- getContents
+    let numbers = toInts userInput
+    print (sum numbers)
