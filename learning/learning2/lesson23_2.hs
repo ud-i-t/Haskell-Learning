e8ba3d9a3454405866db29d90b54369eca4d40a6
ud <mizunoawa.web@gmail.com>
2022-12-20 21:15:26 +0900

    lesson23

--- learning/learning2/lesson23_2.hs
+++ learning/learning2/lesson23_2.hs
@@ -1,54 +1,7 @@
 -- lesson23
 {-# LANGUAGE OverloadedStrings #-} -- 言語拡張: Text型にリテラル値を使用
 import qualified Data.Text as T
-import Data.Semigroup
-
-firstWord :: String
-firstWord = "pessimism"
-
-secondWord :: T.Text
-secondWord = T.pack firstWord
-
-thirdWord :: String
-thirdWord = T.unpack secondWord
-
-fourthWord :: T.Text
-fourthWord = T.pack thirdWord
-
-myWord :: T.Text
-myWord = "dog"
-
-sampleInput :: T.Text
-sampleInput = "this\nis\ninput"
--- T.lines sampleInput でリストに分割
--- T.unlines で戻す
-
-someText :: T.Text
-someText = "Some\ntext for\tyou"
--- T.words someText でリストに分割
--- T.unwords で戻す
-
-breakText :: T.Text
-breakText = "simple"
-
-exampleText :: T.Text
-exampleText = "This is simple to do"
--- T.splitOn breakText exampleText で任意の文字列で分割(この場合"simple")
--- T.intercalate breakText (T.splitOn breakText exampleText) で戻す
-
--- 文字列の連結は++を使えない。mconcatか<>を使用する
-combinedTextMonoid :: T.Text
-combinedTextMonoid = mconcat ["some", " ", "text"]
-
-combinedTextSemigroup :: T.Text
-combinedTextSemigroup = "some" <> " " <> "text"
-
--- quick check 23-3
-myLines :: T.Text -> [T.Text]
-myLines str = T.splitOn "\n" str
-
-myUnlines :: [T.Text] -> T.Text
-myUnlines list = T.intercalate "\n" list
+import qualified Data.Text.IO as TIO
 
 -- 23-3
 keyword :: T.Text
@@ -61,3 +14,6 @@ highlight :: T.Text -> T.Text -> T.Text
 highlight query fullText = T.intercalate highlighted pieces
   where pieces = T.splitOn query fullText
         highlighted = mconcat ["{",query,"}"]
+
+main = do
+  TIO.putStrLn (highlight keyword bgText)
