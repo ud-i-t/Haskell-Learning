87279414b8d585cc992354fc92f68cd0af1a6bdf
ud <mizunoawa.web@gmail.com>
2023-02-03 22:17:49 +0900

    lesson25

--- learning/learning2/lesson25/practice1.hs
+++ learning/learning2/lesson25/practice1.hs
@@ -5,20 +5,16 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import qualified Data.Text.Encoding as E
 
-charCount :: T.Text -> Int
-charCount str = B.length (E.encodeUtf8 str)
+charCount :: B.ByteString -> Int
+charCount str = T.length (E.decodeUtf8 str)
 
-bcInt :: T.Text
-bcInt = "あいうえお"
-
--- これだとUnicodeを読み込めない
 main :: IO ()
 main = do
     args <- getArgs
     let fileName = head args
-    file <- TIO.readFile fileName
-    let l = T.length file
-    -- let c = charCount file
-    print l 
-    -- print c
+    file <- B.readFile fileName
+    let b = B.length file
+    let l = charCount file
+    print b 
+    print l
     print "all done"
