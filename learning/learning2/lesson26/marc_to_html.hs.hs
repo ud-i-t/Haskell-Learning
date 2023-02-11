54d95f351c3ebac78d273a19f6a34a9c51d5b554
ud <mizunoawa.web@gmail.com>
2023-02-11 10:33:23 +0900

    lesson26

--- learning/learning2/lesson26/marc_to_html.hs.hs
+++ learning/learning2/lesson26/marc_to_html.hs.hs
@@ -17,6 +17,31 @@ bookToHTML book = mconcat ["<p>\n", titleInTags, authorInTags, "</p>\n"]
     where titleInTags = mconcat ["<strong>", (title book), "</strong>\n"]
           authorInTags = mconcat ["<em>", (author book), "</em>\n"]
 
+booksToHTML :: [Book] -> Html
+booksToHTML books = mconcat ["<html>\n"
+                            ,"<head><title>books</title></head>"
+                            ,"<meta charset='utf-8'/>"
+                            ,"</head>\n"
+                            ,"<body>\n"
+                            ,booksHTML
+                            ,"\n</body>\n"
+                            ,"</html>"]
+    where booksHTML = (mconcat . (map bookToHTML)) books
+
 book1 :: Book
 book1 = Book { title = "The Conspiracy Against the Human Race"
                ,author = "Ligotti, Thomas"}
+
+book2 :: Book
+book2 = Book { title = "A Short History of Decay"
+               ,author = "Cioran Emil"}
+
+book3 :: Book
+book3 = Book { title = "The Tears of Eros"
+               ,author = "Bataille, Georges"}
+
+myBooks :: [Book]
+myBooks = [book1, book2, book3]
+
+main :: IO ()
+main = TIO.writeFile "books.html" (booksToHTML myBooks)
